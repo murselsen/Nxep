53829c1a33683af60105b8e9a9aa79cda8229220
Mürsel <murselsen_35@hotmail.com>
2024-10-12 13:44:33 +0300

    added main redirect

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
+const profileRoutes = require("./routes/profileRoutes");
+// const config = path.join(__dirname, "config.json");
 
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  resJson = {
+    routes: [
+      { title: "Main", path: "/" },
+      { title: "Profile List", path: "profiles/" },
+    ],
+  };
+  res.json(resJson);
+  console.log("Response:", resJson);
+});
+
+// Profiles Routes
+app.use("/profiles", profileRoutes);
+
+// Sunucuyu başlat
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`The server is running on port ${PORT}.`);
+});
